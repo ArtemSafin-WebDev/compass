92a1513c9fa708919c27ea32e4f224d36180a9a7
Danil Melentev <mejleht@yandex.ru>
2021-09-14 11:40:09 +0300

    gh-pages

--- build/assets/js/backend.js
+++ build/assets/js/backend.js
@@ -30,6 +30,37 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Модалка с оповещением об успехе отправки формы
+    const modal = document.querySelector('.modal-form__inner');
+    if (modal) {
+        const modalForm = modal.querySelector('.modal-form__form');
+        const standardLayer = modal.querySelector('.modal-form__standart-layer');
+        const successLayer = modal.querySelector('.modal-form__success-layer');
+        const backLink = modal.querySelector('.contact-us__form-success-link');
+
+        modalForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            if ($(modalForm).parsley().isValid()) {
+                standardLayer.classList.remove('active');
+                successLayer.classList.add('active');
+                modalForm.reset();
+                var heightChangeEvent = new CustomEvent('heightchange');
+
+                document.dispatchEvent(heightChangeEvent);
+            }
+        });
+
+        backLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            standardLayer.classList.add('active');
+            successLayer.classList.remove('active');
+            var heightChangeEvent = new CustomEvent('heightchange');
+
+            document.dispatchEvent(heightChangeEvent);
+        })
+    }
+
     // hyst modal
     // По клику на красную кнопку "Связаться" закрываем меню
     const btn = document.querySelector('.menu__contact-us');
